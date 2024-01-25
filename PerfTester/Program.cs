06a0417f5af6b0caabc64b33d4871fcc9f730755
Artem Kolisnyk <artkolisnyk@gmail.com>
2024-01-25 22:05:20 +0200

    Remove KeepBenchmarkFiles config

--- PerfTester/Program.cs
+++ PerfTester/Program.cs
@@ -3,8 +3,6 @@ using BenchmarkDotNet.Order;
 using BenchmarkDotNet.Running;
 using PerfTester;
 
-var config = DefaultConfig.Instance
-    .WithOption(ConfigOptions.KeepBenchmarkFiles, false)
-    .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest));
+var config = DefaultConfig.Instance.WithOrderer(new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest));
 
 BenchmarkRunner.Run<LambdaExpressionVsMethodGroupBenchmark>(config);
