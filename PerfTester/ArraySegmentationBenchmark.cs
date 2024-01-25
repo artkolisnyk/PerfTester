79f54a9a1695f38242cba1e4b3b753fb88f714ef
Artem Kolisnyk <artkolisnyk@gmail.com>
2024-01-25 20:08:24 +0200

    Add benchmark for array segmentation

---
+++ PerfTester/ArraySegmentationBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PerfTester;
+
+[MemoryDiagnoser(false)]
+public class ArraySegmentationBenchmark
+{
+    private const int Offset = 1;
+    private const int Count = 2;
+
+    private static readonly string[] Words = { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+
+    [Benchmark]
+    public string[] Linq()
+    {
+        return Words.Skip(Offset).Take(Count).ToArray();
+    }
+
+    [Benchmark]
+    public ArraySegment<string> Segment()
+    {
+        return new ArraySegment<string>(Words, Offset, Count);
+    }
+    
+    [Benchmark]
+    public Span<string> Span()
+    {
+        return Words.AsSpan().Slice(Offset, Count);
+    }
+}
--- PerfTester/Program.cs
+++ PerfTester/Program.cs
@@ -1,10 +1,11 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Order;
 using BenchmarkDotNet.Running;
+using PerfTester;
 using PerfTester.Mappers;
 
 var config = DefaultConfig.Instance
     .WithOption(ConfigOptions.KeepBenchmarkFiles, false)
     .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest));
 
-BenchmarkRunner.Run<MappersBenchmark>(config);
+BenchmarkRunner.Run<ArraySegmentationBenchmark>(config);
